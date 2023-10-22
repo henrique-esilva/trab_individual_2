b5293e0174f6ec353d2b03beaf5ae0dbe83f2907
Pedro Henrique <eduardofilhojj@gmail.com>
2023-10-22 15:00:16 -0300

    22/10/2023

--- main.py
+++ main.py
@@ -3,25 +3,29 @@
 # padrao eX_tX_pX_sX
 
 # uma string de formato padrão para notas
-_format = "e{}_t{}_p{}_s{}"
+_templateStr = "e{}_t{}_p{}_s{}"
 # formato padrão em dicionário
-_form_dict = {"e": 0, "t": 0, "p": 0, "s": 0}
+_templateDict = {"e": 0, "t": 0, "p": 0, "s": 0}
 # lista de candidatos
 candidatos = dict()
 
-print(_format)
-print(candidato1.split(sep="_"))
-
 def get_crude(arg:str):
-	"""Retorna as notas em formato de dicionário.
+	"""\
+Retorna as notas em formato de dicionário.
 Forneça uma string no formato "eX_tX_pX_sX"
 onde cada X é substituído por um número correspondente
 à nota em cada avaliação indicada pela letra"""
-	return {element[:1]: element[1:] for element in arg.split(sep="_")}
+	return {element[:1]: int(element[1:]) for element in arg.split(sep="_")}
 
 
-def get_formated(arg:list):
-	"""Retorna uma string no formato
+def get_dict(arg:list|tuple):
+	a = [i for i in'etps']
+	return {key:arg[a.index(key)] for key in a}
+
+
+def get_formated(arg:dict):
+	"""\
+Retorna uma string no formato
 'eX_tX_pX_sX' onde cada 'X' é a nota obtida na avaliação
 indicada pela letra anterior:
     e = entrevista
@@ -32,12 +36,15 @@ Deve receber um dicionário no formato
 letra: nota_correspondente
 onde
     'letra' deve ser string
-    'nota_correspondente' deve ser inteiro"""
-	return _format.format(arg["e"], arg["t"], arg["p"], arg["s"])
+    'nota_correspondente' deve ser inteiro
+ou uma lista de 4 inteiros correspondentes às notas
+na ordem [e, t, p, s]"""
+	return _templateStr.format(arg["e"], arg["t"], arg["p"], arg["s"])
 
 
 def filtrar_por_notas(etps:list|tuple, vetor:dict):
-	"""Forneça uma lista ou tupla de quatro números inteiros
+	"""\
+Forneça uma lista ou tupla de quatro números inteiros
 e um dicionário no formato
     candidato: notas
 onde
@@ -49,12 +56,10 @@ Retorna um dicionário contendo apenas os elementos do dicionario fornecido
 que possuírem notas todas de valor igual ou superior ao correspondente na
 lista ou tupla fornecida."""
 	aprovados = {}
-	for candidato, notas in vetor.items:
+	for candidato, notas in vetor.items():
 		v = get_crude(notas) # try 'with get_crude(notas) as v: [...]' or something like
-		if not (etps) > (v['e'], v['p'], v['t'], v['x']):
+		if not tuple(etps) > (v['e'], v['p'], v['t'], v['s']):
 			aprovados[candidato] = notas
 	return aprovados
 
 
-print(get_crude(candidato1))
-print(get_formated(get_crude(candidato1)))
--- teste.py
+++ teste.py
@@ -2,6 +2,12 @@
 
 import main
 
-# testar docstrings das funções do módulo principal 'main'
-help(main.get_formated)
-help(main.filtrar_por_notas)
+main.candidatos.update(
+	fernandofernandes=main.get_formated(main.get_dict([2,2,2,3])),
+	almerindaneves=main.get_formated(main.get_dict([2,2,3,3])),
+	alexchaves=main.get_formated(main.get_dict([2,3,3,3])),
+	nandomoura=main.get_formated(main.get_dict([3,3,3,3])),
+	juliamota=main.get_formated(main.get_dict([7,6,6,7])),
+)
+
+print(main.filtrar_por_notas([2,2,3,3], main.candidatos))
